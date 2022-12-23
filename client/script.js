e62dd9598fd65611b090630d1277f1c4eabea12a
alifshah <alifhasanshah@gmail.com>
2022-12-24 01:28:08 +0600

    deploy nodejs to render and updated fetch link

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
     const messageDiv = document.getElementById(uniqueId)
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://codexai-ytee.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
